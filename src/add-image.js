e4ad612bc06f8e2471f0c23fa280195d2b58c81c
Bidisha Das <officialbidisha1@gmail.com>
2022-07-31 12:57:44 +0530

    Adding multiple js and multiple html files

--- src/add-image.js
+++ src/add-image.js
@@ -1,10 +1,10 @@
-import Kiwi from './kiwi.jpg';
+import KiwiImage from './components/kiwi-image/kiwi-image';
 import altText from './altText.txt';
 function addImage(){
     const img = document.createElement('img');
     img.alt = altText;
     img.width = 300;
-    img.src = Kiwi;
+    img.src = KiwiImage;
     const body = document.querySelector('body');
     body.appendChild(img);
 }   
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,85 +1,104 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 /**
  * For clean cache mechanism
  */
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 /**
  * For updating with md5 in html
  */
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   // will start from this point when starting the building process
-  entry: "./src/index.js",
+  // entry: "./src/index.js",
+
+  /**
+   * For two different entry points.
+   */
+  entry: {
+    "hello-world": "./src/hello-world.js",
+    kiwi: "./src/kiwi.js",
+  },
   output: {
     // directory and file where the file should be generated
-    filename: "bundle.[contenthash].js", // to handle browser caching.
+    // filename: "bundle.[contenthash].js", // to handle browser caching.
+    /**
+     * Use filenames from entry points for the bundles
+      * We could also use id, but we want human readable so using this. 
+    */
+    filename: '[name].[contenthash].js',
     // path: 'dist', // this result in absolute path error,
     path: path.resolve(__dirname, "./dist"), // __dirname is the absolute path to the directory containing the source file
-    publicPath: ''
+    publicPath: "",
   },
   mode: "production",
   module: {
-
     // an array of specific rules
     rules: [
       {
         test: /\.(ttf)$/,
-        type: 'asset/resource'
+        type: "asset/resource",
       },
       {
         test: /\.(png|jpg)$/,
-        type: 'asset'
-      }, 
+        type: "asset",
+      },
       {
-        test : /\.(txt)$/,
-        type: 'asset/source'
+        test: /\.(txt)$/,
+        type: "asset/source",
       },
       {
         test: /\.css$/,
-        use:[
+        use: [
           MiniCssExtractPlugin.loader, // this injects the styles produced by css-loader in bundle                                                                                              .js
-          'css-loader' // it just returns the contents of css file after reading it.
-        ]
-
-      }, 
+          "css-loader", // it just returns the contents of css file after reading it.
+        ],
+      },
       {
-        test : /\.js$/,
+        test: /\.js$/,
         exclude: /node_modules/,
-        use:{
-          loader: 'babel-loader',
-          options:{
-            presets: ['@babel/env'],
-            plugins: ['@babel/plugin-proposal-class-properties']
-          }
-        }
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
       },
       {
         test: /\.hbs$/,
-        use: [
-          'handlebars-loader'
-        ]
-      }
-    ]
+        use: ["handlebars-loader"],
+      },
+    ],
   },
 
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'styles.[contenthash].css'
+      filename: "styles.[contenthash].css",
     }),
-    new CleanWebpackPlugin(
+    new CleanWebpackPlugin(),
     //   {
     //   cleanOnceBeforeBuildPatterns:[
     //     // '**/*',
     //   ]
     // }
-    ),
     new HtmlWebpackPlugin({
-      title: 'Hello World',
-      template:'src/index.hbs',
-      description: 'Some description',
-      
-    })
-  ]
+      filename: 'hello-world.html',
+      chunks: [
+          'hello-world'
+      ],
+      title: "Hello World",
+      template: "src/page-template.hbs",
+      description: "Hello world",
+    }),
+
+    new HtmlWebpackPlugin({
+      filename: 'kiwi.html',
+      chunks: ['kiwi'],
+      title: "Kiwi",
+      template: "src/page-template.hbs",
+      description: "Kiwi",
+    }),
+  ],
 };
